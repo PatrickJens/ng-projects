e65ef16ab2cc992af2984b3b7688c35835e7f82e
Patrick Cannell <pcannell14@gmail.com>
2026-03-19 16:59:39 -0500

    components interfaces 01

--- appointment-app/src/app/app-module.ts
+++ appointment-app/src/app/app-module.ts
@@ -2,10 +2,12 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { AppointmentList } from './appointment-list/appointment-list';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    AppointmentList
   ],
   imports: [
     BrowserModule
