bbf475b953c69ea04ce1990a1fe216682a7f8dcd
Patrick Cannell <pcannell14@gmail.com>
2026-03-24 16:55:02 -0500

    book.model.ts

--- book-mgmt-system/src/app/app-module.ts
+++ book-mgmt-system/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { BookModelTs } from './models/book.model.ts/book.model.ts';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    BookModelTs
   ],
   imports: [
     BrowserModule,
