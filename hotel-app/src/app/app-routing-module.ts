73b689dce396c1f5512d17e75e4722a4eb0bca0c
Patrick Cannell <pcannell14@gmail.com>
2026-03-31 16:51:12 -0500

    routing-02

--- hotel-app/src/app/app-routing-module.ts
+++ hotel-app/src/app/app-routing-module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {Home} from './home/home'
+import { ReservationList } from './reservation-list/reservation-list';
+import { ReservationForm } from './reservation-form/reservation-form';
 
 const routes: Routes = [
-  {path:"", component: Home}
+  {path:"", component: Home},
+  {path:"list", component: ReservationList},
+  {path:"new", component: ReservationForm},
 ];
 
 @NgModule({
